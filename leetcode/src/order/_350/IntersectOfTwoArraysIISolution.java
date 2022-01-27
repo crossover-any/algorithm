feb52603bfcaa33898347a95537db686932bb4dc
tengxq <tengxingqiang@ouchteam.com>
2022-01-27 17:43:31 +0800

    add 53 & 349 &350

---
+++ leetcode/src/order/_350/IntersectOfTwoArraysIISolution.java
@@ -0,0 +1,136 @@
+package order._350;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 两个数组的交集 II
+ *
+ * 给你两个整数数组nums1 和 nums2 ，请你以数组形式返回两数组的交集。
+ * 返回结果中每个元素出现的次数，应与元素在两个数组中都出现的次数一致
+ * （如果出现次数不一致，则考虑取较小值）。可以不考虑输出结果的顺序。
+ *
+ *
+ * @author tengxq
+ */
+public class IntersectOfTwoArraysIISolution {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 思路：
+     *      1.初始化nums1为map{value, counter(出现次数)}字典形式
+     *      2.遍历nums2如果包含当前value并且counter大于0则符合查找条件
+     *
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(n)
+     *
+     * @param nums1 数组1
+     * @param nums2 数组2
+     * @return 交集数组
+     */
+    public int[] intersect1(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null) {
+            return new int[]{};
+        }
+        Map<Integer, Integer> dic = new HashMap<>(nums1.length);
+        for (int num : nums1) {
+            Integer counter = dic.getOrDefault(num, 0);
+            dic.put(num, counter + 1);
+        }
+        int[] arr = new int[Math.min(nums1.length, nums2.length)];
+        int index = 0;
+        for (int num : nums2) {
+            if (dic.containsKey(num)) {
+                Integer counter = dic.get(num);
+                if (counter > 0) {
+                    arr[index++] = num;
+                    dic.put(num, counter -1);
+                }
+            }
+        }
+        return Arrays.copyOfRange(arr, 0, index);
+    }
+
+    /**
+     * 思路：
+     *      1.使用快排分别把两个数组从小到大（从大到小）排列
+     *      2.遍历两数组进行比较
+     *
+     * 时间复杂度：O(n*logn)
+     * 空间复杂度：O(logn)
+     *
+     * @param nums1 数组1
+     * @param nums2 数组2
+     * @return 交集数组
+     */
+    public int[] intersection2(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null) {
+            return new int[]{};
+        }
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int index1 = 0;
+        int index2 = 0;
+        // 这里集合需要比较上一个的值就直接初始化一个数组
+        int index = 0;
+        // 取最小是为了节约空间，多申请一个空间的目的是使用index=0的位置减少if判断
+        int[] arr = new int[(Math.min(nums1.length, nums2.length)) + 1];
+
+        while (index1 < nums1.length && index2 < nums2.length) {
+            if (nums1[index1] == nums2[index2]) {
+                // 相对于349题这里我们不再需要去重，直接把if判断去掉即可
+                arr[++index] = nums1[index1];
+                index1++;
+                index2++;
+            } else if (nums1[index1] > nums2[index2]) {
+                index2++;
+            } else {
+                index1++;
+            }
+        }
+        return Arrays.copyOfRange(arr, 1, index + 1);
+    }
+
+    /**
+     * 思路：
+     *      1.使用快排分别把两个数组从小到大（从大到小）排列
+     *      2.遍历两数组进行比较
+     *
+     * 时间复杂度：O(n*logn)
+     * 空间复杂度：O(logn)
+     *
+     * @param nums1 数组1
+     * @param nums2 数组2
+     * @return 交集数组
+     */
+    public int[] intersect3(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null) {
+            return new int[]{};
+        }
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int index1 = 0;
+        int index2 = 0;
+        // 这里集合需要比较上一个的值就直接初始化一个数组
+        int index = 0;
+        // 取最小是为了节约空间，这里不再需要去重，index指向尾部元素的下一个位置下标，不用多申请一个空间
+        int[] arr = new int[Math.min(nums1.length, nums2.length)];
+
+        while (index1 < nums1.length && index2 < nums2.length) {
+            if (nums1[index1] == nums2[index2]) {
+                // 这里的index指向的数组中的尾部元素的下一个位置下标
+                arr[index++] = nums1[index1];
+                index1++;
+                index2++;
+            } else if (nums1[index1] > nums2[index2]) {
+                index2++;
+            } else {
+                index1++;
+            }
+        }
+        return Arrays.copyOfRange(arr, 0, index);
+    }
+}
---
+++ leetcode/src/order/_53/MaximumSubarrySolution.java
@@ -0,0 +1,67 @@
+package order._53;
+
+/**
+ * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ * <p>
+ * 子数组 是数组中的一个连续部
+ *
+ * @author tengxq
+ */
+public class MaximumSubarrySolution {
+    public static void main(String[] args) {
+        int[] nums = new int[]{5, 4, -1, 7, 8};
+        MaximumSubarrySolution maxSubArraySolution = new MaximumSubarrySolution();
+        maxSubArraySolution.maxSubArray1(nums);
+    }
+
+    /**
+     * 思路：假设最大子数组和是从第i个位置开始，我们只需要遍历每个以i位置开始的连续元素的最大和
+     *
+     * 时间复杂度：O(n^2)
+     * 空间复杂度：O(1)
+     *
+     * @param nums 数组
+     * @return 最大子数组和
+     */
+    public int maxSubArray1(int[] nums) {
+        int maxSub = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            int max = 0;
+            for (int j = i; j < nums.length; j++) {
+                max += nums[j];
+                if (maxSub < max) {
+                    maxSub = max;
+                }
+            }
+        }
+        return maxSub;
+    }
+
+    /**
+     * 思路：同样的将问题分割最小
+     *       假设只有一个数，那么我们的最大子数组和就是第一个数（maxSub=nums[0]）
+     *       如果有第二个数，我们将第一个数加上第二个数的和与第一个数进行比较
+     *       如果比第一个数的和要大，那么我们可以将第二个数收入囊中
+     *           反之说明第一个数是目前为止找到最大的子数组，但是我们还得继续找下去
+     *           将目前最大值记录下来，设置最大和为第二个元素以第二个元素为起点继续查找
+     *
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
+     *
+     * @param nums 数组
+     * @return 最大子数组和
+     */
+    public int maxSubArray2(int[] nums) {
+        int maxSub = nums[0];
+        int pres = maxSub;
+
+        for (int i = 1; i < nums.length; i++) {
+            int next = pres + nums[i];
+            pres = Math.max(nums[i], next);
+            if (pres > maxSub) {
+                maxSub = pres;
+            }
+        }
+        return maxSub;
+    }
+}
