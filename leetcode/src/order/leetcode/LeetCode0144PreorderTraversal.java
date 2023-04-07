bb378d5e20922afc0253736bc7a4c884aa69854b
tengxq <tengxingqiang@ouchteam.com>
2023-04-07 10:07:03 +0800

    add: 二叉树的前序遍历

---
+++ leetcode/src/order/leetcode/LeetCode0144PreorderTraversal.java
@@ -0,0 +1,75 @@
+package order.leetcode;
+
+import entity.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * 二叉树的前序遍历
+ *
+ * @author tengxq
+ */
+public class LeetCode0144PreorderTraversal {
+
+    /**
+     * 前序遍历-递归
+     *
+     * @param root 根节点
+     * @return 排序列表
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        preOrder(root, list);
+        return list;
+    }
+
+    public void preOrder(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        list.add(root.val);
+        preOrder(root.left, list);
+        preOrder(root.right, list);
+    }
+
+    /**
+     * 前序遍历-非递归
+     *
+     * @param root 根节点
+     * @return 排序列表
+     */
+    public List<Integer> preorderTraversal2(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            list.add(node.val);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeNode3 = new TreeNode(3);
+        TreeNode treeNode1 = new TreeNode(1);
+        TreeNode treeNode2 = new TreeNode(2);
+        treeNode3.left = treeNode1;
+        treeNode3.right = treeNode2;
+        LeetCode0144PreorderTraversal solution = new LeetCode0144PreorderTraversal();
+        System.out.println(solution.preorderTraversal2(treeNode3));
+    }
+}
