bc8d11a9c3dff51677fd6e4669adba98dbae91c8
tengxq <tengxingqiang@ouchteam.com>
2023-04-12 14:34:46 +0800

    add: 每日温度

---
+++ leetcode/src/order/leetcode/LeetCode0739DailyTeperatures.java
@@ -0,0 +1,39 @@
+package order.leetcode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 给定一个整数数组temperatures，表示每天的温度，返回一个数组answer，
+ * 其中answer[i]是指对于第 i 天，下一个更高温度出现在几天后。如果气温在这之后都不会升高，请在该位置用0 来代替。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * 输入: temperatures = [73,74,75,71,69,72,76,73]
+ * 输出:[1,1,4,2,1,1,0,0]
+ * 示例 2:
+ *
+ * 输入: temperatures = [30,40,50,60]
+ * 输出:[1,1,1,0]
+ * 示例 3:
+ *
+ * 输入: temperatures = [30,60,90]
+ * 输出: [1,1,0]
+ * @author tengxq
+ */
+public class LeetCode0739DailyTeperatures {
+
+    public int[] dailyTemperatures(int[] temperatures) {
+        Deque<Integer> stack = new LinkedList<>();
+        int[] ans = new int[temperatures.length];
+        for (int i = 0, len = temperatures.length; i < len; i++) {
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                ans[stack.peek()] = i - stack.pop();
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
+}
